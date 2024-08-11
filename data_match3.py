f0f0818d48eb7c5a3dfa9a6e3343a266948b141a
rokosbasilisk <ram.bharadwaj.arya@gmail.com>
2024-08-11 19:23:43 +0530

    update .

---
+++ data_match3.py
@@ -0,0 +1,72 @@
+from data_utils import GenerateMatch3Dataset
+import argparse
+
+def rand_cot(inputs, solution_list,): #rand_schedule_cot_string
+    '''
+    To string for CoT which randomly include one position of the 2SUM summands
+    '''
+    st = ' ' + ' '.join([cat_row(row) for row in inputs])
+    st += ' P '
+    for ind1,subsoln in solution_list:
+        if subsoln[0]=='-':
+            st += ind1+'-'+' '+str(subsoln[1:])+'-'+' '
+        elif subsoln[0]=='F':
+            st += ind1+'-'+' '+subsoln[1:]+' '
+        else:
+            print(subsoln[-1])
+            raise ValueError('Unexpected subsoln')
+    st += 'A ' + str(any(['-' in s[-1] for s in solution_list]))
+    return st
+
+def serial_cot(inputs, solution_list,):
+    st = ' ' + ' '.join([cat_row(row) for row in inputs])
+    st += ' P '
+    for subsoln in solution_list[0:-1]:
+        st += subsoln+' '
+    st += 'A ' + solution_list[-1]
+    return st
+
+STRING_FUNCTION_MAP =  {
+        'rand_cot': rand_cot,
+        'serial': serial_cot,
+    }
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate Match3 String CSVs and hyper parameter json')
+    
+    parser.add_argument('--name', type=str, required=True, help='Name of the dataset')
+    parser.add_argument('--train_samples', type=float, default=1e7, help='Number of training samples, to convert to int')
+    parser.add_argument('--test_samples', type=float, default=2e3, help='Number of test samples, to convert to int')
+
+    parser.add_argument('--dimension', type=int, default=3, help='Dimension for Match3 class')
+    parser.add_argument('--mod', type=int, default=10, help='Mod value')
+    parser.add_argument('--length', type=int, default=10, help='Length of data')
+    parser.add_argument('--true_instance_rate', type=float, default=0.5, help='True instance dataset percentage')
+    parser.add_argument('--cot_rate', type=float, default=0.5, help='cot dataset percentage')
+    parser.add_argument('--no_filler_rate', type=float, default=0, help='No intermediate tokens dataset percentage')
+    parser.add_argument('--corruption_rate', type=float, default=4/3, help='Corruption dataset percentage for False instances, Geometric with this rate')
+    
+    parser.add_argument('--cot_to_string', type=str, choices=['rand_cot', 'serial'], default='rand_cot', help='Function for cot string transformation')
+
+    parser.add_argument('--data_path', type=str, default='./data/', help='Path to save the dataset')
+
+    args = parser.parse_args()
+
+    GenerateMatch3Dataset(
+        name=args.name,
+        train_samples=int(args.train_samples),
+        test_samples=int(args.test_samples),
+        dimension=args.dimension,
+        mod=args.mod,
+        length=args.length,
+        true_instance_rate=args.true_instance_rate,
+        cot_rate=args.cot_rate,
+        no_filler_rate=args.no_filler_rate,
+        corruption_rate=args.corruption_rate,
+        cot_to_string=STRING_FUNCTION_MAP[args.cot_to_string],
+        data_path=args.data_path
+    )
+    return
+
+if __name__ == '__main__':
+    main()
